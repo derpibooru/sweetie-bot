1ad5d0caa00fb20684c87971a069c35be3b522c4
Luna <cod7777@yandex.ru>
2019-10-16 14:01:51 +0200

    display aliases

--- lib/commands/tag.rb
+++ lib/commands/tag.rb
@@ -7,6 +7,7 @@ CommandDispatcher.register name: 'tag' do |msg, args|
     text = "https://derpibooru.org/tags/#{data.tag.slug} - #{data.tag.images} images are tagged '#{data.tag.name}'.\n"
   end
   text += "#{data.tag.short_description}\n" if data.tag.short_description.present?
+  text += "Aliases: #{data.aliases.join(', ')}" unless data.aliases.empty?
   text += "Implies: #{data.tag.implied_tags.join(', ')}" unless data.tag.implied_tags.empty?
 
   msg.reply with: text, mention: false
