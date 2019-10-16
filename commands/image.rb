74c51c74f823029c204736d354850de8d222ca4f
Luna D <cod7777@yandex.ru>
2019-10-16 23:36:08 +0200

    did i forget to save

--- commands/image.rb
+++ commands/image.rb
@@ -1,27 +1,7 @@
 # frozen_string_literal: true
 
-class ChatImage
-  def self.handle(msg)
-    match_data = msg.text.match />>([0-9]+)/
-
-    if match_data
-      ChatImage.send_image(msg, match_data[1])
-      SweetieBot.log "image (#{match_data[1]}) from #{msg.sender.username}"
-    end
-  end
-
-  def self.send_image(msg, id)
-    data = Booru.image(id)
-
-    if data
-      Image.embed data, message: msg
-      return true
-    end
-  end
-end
-
 CommandDispatcher.register name: 'image' do |msg, args|
-  msg.reply 'no such image!' unless ChatImage.send_image(msg, args.raw)
+  msg.reply 'no such image!' unless Image.send_image(msg, args.raw)
 end
 
 CommandDispatcher.alias 'i', 'image'
