7fe3505827f39b40f5dd4e608e046607f931e70a
Luna <cod7777@yandex.ru>
2019-10-18 16:28:57 +0200

    throw StandardError and DRY

--- models/quote.rb
+++ models/quote.rb
@@ -3,25 +3,29 @@
 class Quote < ActiveRecord::Base
   validates :body, uniqueness: { scope: [:user, :channel], message: 'quote already exists' }
 
-  class NotFound < StandardError
-  end
-
-  class InvalidID < StandardError
-  end
-
   def self.add(user, channel, body)
     Quote.create! user: user, channel: channel, body: body.strip
   end
 
-  def self.remove(user, id)
-    user_quotes = Quote.where user: user
+  def self.search(**args)
+    id = args[:id] || 1
 
-    raise InvalidID, 'ID out of range' if id.to_i <= 0 || user_quotes.count < id
+    quotes = Quote.where args[:field] => args[:value]
 
-    if (q = user_quotes.order(created_at: :asc).offset(id - 1).first)
-      q.destroy!
+    raise 'ID out of range' if id.to_i <= 0 || quotes.count < id
+
+    if (q = quotes.order(created_at: :asc).offset(id - 1).first)
+      q
     else
-      raise NotFound, 'quote not found'
+      raise 'quote not found'
     end
+  end
+
+  def self.remove(user, id)
+    search(field: :user, value: user, id: id).destroy!
+  end
+
+  def self.remove_from_channel(chan, id)
+    search(field: :channel, value: chan, id: id).destroy!
   end
 end
