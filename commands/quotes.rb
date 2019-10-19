52bc1b618f0fc3f4e5ad917f4a63a8bf98a41262
Luna D <cod7777@yandex.ru>
2019-10-19 16:03:23 +0200

    fix quotes

--- commands/quotes.rb
+++ commands/quotes.rb
@@ -10,7 +10,7 @@ CommandDispatcher.register name: 'quotes', help_text: 'displays the amount of qu
   end
 
   begin
-    _, _, count = Quote.search field: field, value: subject, id: quote_id
+    _, _, count = Quote.search field: field, value: subject, id: 1
     msg.reply "#{subject} has **#{count}** quote#{count != 1 ? 's' : ''} on record."
   rescue StandardError => ex
     msg.reply ex.message
