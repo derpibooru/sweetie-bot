833cd923c738a8a27a68a58bf7307a115be1e061
Luna D <cod7777@yandex.ru>
2020-02-13 21:04:54 +0100

    add this for shits and giggles

---
+++ handlers/mom.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RANDOM_PHRASES = [
+  "it's you, of course.",
+  'you <3',
+  "there is a high probability it's you.",
+  "it's Celestia! Um... I mean you! hehehehe ><",
+  'might be you.',
+  'definitely you :3',
+  'obviously you.',
+  'the person I am pinging :3'
+].freeze
+
+SweetieBot.instance.handler do |msg|
+  next unless msg.sender.id.to_s == '128567958086615040'
+
+  lower = msg.text.downcase
+
+  if lower.start_with?('sweetie who is your') ||
+     lower.start_with?('sweetie who is your') ||
+     lower.start_with?('sweetie who is the best') ||
+     lower.start_with?('sweetie who is best') ||
+     lower.start_with?('sweetie who is the cutest')
+    msg.reply RANDOM_PHRASES.sample
+
+    true
+  else
+    false
+  end
+end
