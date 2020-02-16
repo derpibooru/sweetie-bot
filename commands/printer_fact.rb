8d6f5b074212241f1127cf8f1f79a78a923072b7
Luna D <cod7777@yandex.ru>
2020-02-16 20:01:21 +0100

    printer facts

--- commands/printer_fact.rb
+++ commands/printer_fact.rb
@@ -3,29 +3,10 @@
 require 'net/http'
 require 'json'
 
+PRINTER_FACTS = JSON.parse(File.read('config/printer_facts.json')).freeze
+
 CommandDispatcher.register name: 'printerfact', help_text: 'displays a random fact about printers' do |msg, _|
-  res = Net::HTTP.get_response(URI('https://catfact.ninja/fact'))
-
-  next unless res
-
-  fact = JSON.parse(res.body)['fact']
-
-  next unless fact
-
-  fact =
-    fact
-    .gsub(/(cat|feline)/i, 'printer')
-    .gsub(/kitten/i, 'baby printer')
-    .gsub(/(lion|leopard|lynx|cheetah)/i, 'big printer')
-    .gsub(/\.(\s*)(printer|baby|big)/) do |_match|
-      # rubocop:disable Style/PerlBackrefs
-      ".#{$1}#{$2.capitalize}"
-      # rubocop:enable Style/PerlBackrefs
-    end
-
-  fact = fact.capitalize
-
-  msg.reply with: fact, mention: false
+  msg.reply with: PRINTER_FACTS.sample, mention: false
 end
 
 CommandDispatcher.alias 'pf', 'printerfact'
