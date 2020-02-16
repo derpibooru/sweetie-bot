7a39f74d22ae24e4b5671f15a3c5fdf55c503859
Luna D <cod7777@yandex.ru>
2020-02-16 20:06:20 +0100

    V A R I E T Y

--- commands/rickroll.rb
+++ commands/rickroll.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
+RICKROLL_LINKS = [
+  'https://www.youtube.com/watch?v=cvh0nX08nRw',
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  'https://www.youtube.com/watch?v=j5a0jTc9S10',
+  'https://www.youtube.com/watch?v=dPmZqsQNzGA',
+  'https://www.youtube.com/watch?v=xfr64zoBTAQ'
+].freeze
+
 CommandDispatcher.register name: 'rickroll' do |msg, _|
-  msg.reply '<https://www.youtube.com/watch?v=dQw4w9WgXcQ>', mention: false
+  msg.reply "<#{RICKROLL_LINKS.sample}>", mention: false
 end
 
 CommandDispatcher.alias 'dailyvideo', 'rickroll'
 CommandDispatcher.alias 'bestvideo', 'rickroll'
 CommandDispatcher.alias 'gen5', 'rickroll'
 CommandDispatcher.alias 'nudes', 'rickroll'
+CommandDispatcher.alias 'send nudes', 'rickroll'
