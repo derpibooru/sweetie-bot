509af2df16fe0ae6267293ed16348f22b4c99695
Luna D <cod7777@yandex.ru>
2020-07-21 22:49:59 +0200

    derp

--- db/schema.sql
+++ db/schema.sql
@@ -8,4 +8,3 @@ CREATE INDEX "index_users_on_admin" ON "users" ("admin");
 CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
 CREATE UNIQUE INDEX "index_quotes_on_body_and_user" ON "quotes" ("body", "user");
-
