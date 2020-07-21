0acb269b81c125bab6cad3af6c40b2c3bba7fa59
Luna D <cod7777@yandex.ru>
2020-07-21 22:51:56 +0200

    oh god i am such a pancake

--- commands/count.rb
+++ commands/count.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 CommandDispatcher.register name: 'count', help_text: 'displays the amount of images that match a given search query' do |msg, args|
-  search_result = Booru.search query: args.raw, filter: '56027'
+  search_result = Booru.search query: args.raw, filter: SweetieBot.config.booru.everything_filter
 
   msg.reply with: "**#{search_result.total}** images match query '_#{args.raw}_'.", mention: false
 end
